a630e3e0249318175c1dd5314dbf9d4a42cdf545
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-28 20:13:25 +0600

    learning appBar title property

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return  MaterialApp(
         title: 'First Mobile App',
-        color: Colors.blue,
+        color: Colors.teal,
         theme: ThemeData(primarySwatch: Colors.green),
         darkTheme: ThemeData(primarySwatch: Colors.blueGrey),
         debugShowCheckedModeBanner: false,
@@ -30,8 +30,23 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-     appBar: AppBar(title: Text('My App')),
+     appBar: AppBar(
+       title: Text('My App'),
+       titleSpacing: 0,
+       // titleTextStyle: ,
+       centerTitle: true,
+       elevation: 0,
+       backgroundColor: Colors.teal,
+       toolbarHeight: 60.0,
+       toolbarOpacity: 1,
+       // toolbarTextStyle: ,
+
+     ),
      body: Text('This is my first App'),
+     // drawer: (),
+     // endDrawer: (),
+     // floatingActionButton: ,
+     // bottomNavigationBar: (),
    );
   }
 
