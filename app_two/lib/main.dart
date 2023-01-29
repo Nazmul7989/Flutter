d684c3a645512872566bfbd6a1843fe3ff12512b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-01-29 21:42:05 +0600

    floating button and bottom navigation bar

--- app_two/lib/main.dart
+++ app_two/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: 'Second Flutter App',
+      color: Colors.teal,
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData(primarySwatch: Colors.grey),
       debugShowCheckedModeBanner: false,
@@ -43,6 +44,44 @@ class HomePage extends StatelessWidget {
             IconButton(onPressed: (){MySnackBar('Click on Search', context);}, icon: Icon(Icons.search)),
             IconButton(onPressed: (){MySnackBar('Click on Notification', context);}, icon: Icon(Icons.notifications)),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        child: Icon(Icons.add),
+        onPressed: (){
+          MySnackBar('I am floating action button', context);
+        },
+      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   backgroundColor: Colors.teal,
+      //   label: Text('Save'),
+      //   icon: Icon(Icons.add),
+      //   onPressed: (){
+      //     MySnackBar('I am floating action button', context);
+      //   },
+      // ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),label: 'Carts'),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
+        ],
+        onTap: (int index){
+          if(index == 0){
+            MySnackBar('Bottom Home menu', context);
+          }
+          if(index == 1){
+            MySnackBar('Bottom Carts menu', context);
+          }
+          if(index == 2){
+            MySnackBar('Bottom Profile menu', context);
+          }
+        },
+        currentIndex: 0,
+        backgroundColor: Colors.teal,
+        selectedItemColor: Colors.white,
+        iconSize: 30,
+        elevation: 0,
       ),
     );
   }
